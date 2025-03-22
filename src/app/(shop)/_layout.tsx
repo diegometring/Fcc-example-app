b622b9c6bfb9c196701273973ae0fc21a6a0b06d
diegometring <diegometringteixeira@gmail.com>
2025-03-22 18:52:45 -0300

    .

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,15 +1,37 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { SafeAreaView } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView} from "react-native-safe-area-context";
+
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name']
+  color: string 
+}) {
+  return <FontAwesome size={24} {...props} style={{ color: '#1BC464'}}/>
+}
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView>
-      <Tabs>
+    <SafeAreaView edges={['top']} style={styles.safeArea}>
+      <Tabs screenOptions={{
+        tabBarActiveTintColor: '#1BC464',
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: {fontSize: 16},
+        tabBarStyle: {
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          paddingTop: 10,
+        }
+      }}>
         <Tabs.Screen 
-            name="index" options={{headerShown: false}} 
+            name="index" options={{tabBarIcon(props) {
+              return <TabBarIcon {...props} name='shopping-cart'/>
+            },
+          }} 
         />
         <Tabs.Screen
-            name="orders" options={{headerShown: false}}
+            name="orders" options={{}}
         />
       </Tabs>
     </SafeAreaView>
@@ -17,3 +39,10 @@ const TabsLayout = () => {
 };
 
 export default TabsLayout;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+
+  }
+})
---
+++ src/app/(shop)/order/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Order = () => {
+  return (
+    <View>
+      <Text>Order</Text>
+    </View>
+  )
+}
+
+export default Order
+
+const styles = StyleSheet.create({})
